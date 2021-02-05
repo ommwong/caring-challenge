cce932f09ca0763e44ba131b9b47910654eeee1c
ommwong <mowong1987@gmail.com>
2021-02-05 15:14:48 +0100

    feat: server starting

--- server/index.js
+++ server/index.js
@@ -1,24 +1,15 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
+const grpc = require('grpc'); //grpc library
+const protoLoader = require('@grpc/proto-loader'); //compiles proto into JS, etc.
 
-const packageDefinition = protoLoader.loadSync('literary.proto', {});
+const PROTO_PATH = "./literary.proto";
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {});
+
+//load packageDefinition into grpc object
 const grpcObject = grpc.loadPackageDefinition(packageDefinition);
 const literaryPackage = grpcObject.literaryPackage;
 
 const server = new grpc.Server();
-
-//uses http2 as transport mechanism; needs credentials; bypasses credentials with createInsecure
-server.bind('localhost:4000', grpc.ServerCredentials.createInsecure());
-
-//communicate service with server
-server.addService(literaryPackage.AuthorService.service, {
-  "createAuthor": createAuthor,
-  "getAuthors": getAuthors,
-  "editAuthor": editAuthor,
-  "deleteAuthor": deleteAuthor
-});
-
-server.start();
 
 //call = {}; not an actual request, a whole call
 //callback = sends response to client
@@ -37,3 +28,16 @@ const editAuthor = (call, callback) => {
 const deleteAuthor = (call, callback) => {
 
 };
+
+//uses http2 as transport mechanism; needs credentials; bypasses credentials with createInsecure
+//communicate service with server
+server.addService(literaryPackage.AuthorService.service, {
+  "createAuthor": createAuthor,
+  "getAuthors": getAuthors,
+  "editAuthor": editAuthor,
+  "deleteAuthor": deleteAuthor
+});
+
+server.bind('localhost:4000', grpc.ServerCredentials.createInsecure());
+console.log("Server running at http://localhost:4000");
+server.start();
