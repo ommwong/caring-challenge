b550f387fa51ae7eee2d9fcc91e6696510b4419a
ommwong <mowong1987@gmail.com>
2021-02-10 12:02:33 +0100

    feat: bodyparser

--- client/index.js
+++ client/index.js
@@ -1,6 +1,23 @@
 const http = require('http');
 
-const server = http.createServer(() => {});
+const router = require('./router/router');
+
+const server = http.createServer((request, response) => {
+  bodyParser(request, response, router)
+});
+
+const bodyParser = (request, response, next) => {
+  if (request.body === 'POST') {
+    const body = [];
+    request.on('data', (chunk) => body.push(chunk));
+    request.on('end', () => {
+      request.body = JSON.parse(Buffer.concat(body));
+      next(request, response);
+    })
+  } else {
+    next(request, response);
+  }
+};
 
 const hostname = 'localhost';
 const port = 3000;
